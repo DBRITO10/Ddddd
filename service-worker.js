f4f212aaa5f2c60c29a11fee7a8ee806225bd2d5
Douglas Brito Da Rocha <oceaniluminat@gmail.com>
2025-08-20 21:49:57 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
